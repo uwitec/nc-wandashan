58d1d6118c0ca00d8b7c870adc24f7137bbcb68f
liuyushi_alice <liuyushi_alice@163.com>
2011-03-28 13:29:40 +0000

    spf

--- wds2/src/client/nc/ui/load/tag/MyClientUI.java
+++ wds2/src/client/nc/ui/load/tag/MyClientUI.java
@@ -1,6 +1,8 @@
 package nc.ui.load.tag;
 
+import nc.ui.pub.ButtonObject;
 import nc.ui.trade.bill.AbstractManageController;
+import nc.ui.trade.button.IBillButton;
 import nc.ui.trade.manage.BillManageUI;
 import nc.vo.pub.CircularlyAccessibleValueObject;
 
@@ -37,7 +39,15 @@ public class MyClientUI extends BillManageUI {
 
 	@Override
 	protected void initSelfData() {
-		//++++++++++
+		ButtonObject btn = getButtonManager().getButton(IBillButton.Line);
+		if (btn != null) {
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.CopyLine));
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.PasteLine));
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.InsLine));
+		}
 
 	}
 
