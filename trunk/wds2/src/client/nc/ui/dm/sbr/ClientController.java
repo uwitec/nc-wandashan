3d2dde3f68da5059d6701d7d48f2659073249c42
liuyushi_alice@163.com <liuyushi_alice@163.com@5166a296-a8aa-909f-7fcb-6b20ff67c8c9>
2011-03-30 05:02:54 +0000

    spf

---
+++ trunk/wds2/src/client/nc/ui/dm/sbr/ClientController.java
@@ -0,0 +1,132 @@
+package nc.ui.dm.sbr;
+
+import nc.ui.trade.bill.AbstractManageController;
+import nc.ui.trade.button.IBillButton;
+import nc.vo.dm.sb.SbDocVO;
+import nc.vo.pub.SuperVO;
+import nc.vo.trade.pub.HYBillVO;
+
+public class ClientController extends AbstractManageController {
+
+	public String[] getCardBodyHideCol() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int[] getCardButtonAry() {
+		return new int[]{
+				IBillButton.Add,
+				IBillButton.Save,
+				IBillButton.Query,
+				IBillButton.Delete,
+				IBillButton.Cancel,
+				IBillButton.Edit,
+				IBillButton.Refresh,
+				IBillButton.Return
+				
+			};
+	}
+
+	public boolean isShowCardRowNo() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean isShowCardTotal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getBillType() {
+		// TODO Auto-generated method stub
+		return "wds_sbdocr";
+	}
+
+	public String[] getBillVoName() {
+		return new String[]{
+				HYBillVO.class.getName(),
+				SbDocVO.class.getName(),
+				SuperVO.class.getName()
+		};
+	}
+
+	public String getBodyCondition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBodyZYXKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getBusinessActionType() {
+		// TODO Auto-generated method stub
+		return nc.ui.trade.businessaction.IBusinessActionType.BD;
+	}
+
+	public String getChildPkField() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public String getHeadZYXKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPkField() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public Boolean isEditInGoing() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isExistBillStatus() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isLoadCardFormula() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public String[] getListBodyHideCol() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int[] getListButtonAry() {
+		return new int[]{
+				IBillButton.Add,
+				IBillButton.Delete,
+				IBillButton.Query,
+				IBillButton.Edit,
+				IBillButton.Save,
+				IBillButton.Refresh,
+				IBillButton.Card
+		};
+	}
+
+	public String[] getListHeadHideCol() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isShowListRowNo() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean isShowListTotal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
---
+++ trunk/wds2/src/client/nc/ui/dm/sbr/ClientUI.java
@@ -0,0 +1,74 @@
+package nc.ui.dm.sbr;
+
+import nc.ui.pub.ButtonObject;
+import nc.ui.trade.bill.AbstractManageController;
+import nc.ui.trade.button.IBillButton;
+import nc.ui.trade.manage.BillManageUI;
+import nc.ui.trade.manage.ManageEventHandler;
+import nc.vo.pub.CircularlyAccessibleValueObject;
+
+/**Ãÿ ‚“µŒÒ
+ * @author Administrator
+ * 
+ */
+public class ClientUI extends BillManageUI {
+
+	@Override
+	protected AbstractManageController createController() {
+		// TODO Auto-generated method stub
+		
+		return new ClientController();
+	}
+	
+	protected ManageEventHandler createEventHandler() {
+		return new ClientEventHandler(this, getUIControl());
+	}
+	
+	@Override
+	public void setBodySpecialData(CircularlyAccessibleValueObject[] vos)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setHeadSpecialData(CircularlyAccessibleValueObject vo,
+			int intRow) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setTotalHeadSpecialData(CircularlyAccessibleValueObject[] vos)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initSelfData() {
+		ButtonObject btn = getButtonManager().getButton(IBillButton.Line);
+		if (btn != null) {
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.CopyLine));
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.PasteLine));
+			btn.removeChildButton(getButtonManager().getButton(
+					IBillButton.InsLine));
+		}
+
+	}
+
+	@Override
+	public void setDefaultData() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return super.getTitle();
+	}
+	
+
+}
