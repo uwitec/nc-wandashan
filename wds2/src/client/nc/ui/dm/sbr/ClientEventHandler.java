a86cc8da4803827dcb20c6a5311048bd301d601c
liuyushi_alice <liuyushi_alice@163.com>
2011-03-30 05:02:54 +0000

    spf

---
+++ wds2/src/client/nc/ui/dm/sbr/ClientEventHandler.java
@@ -0,0 +1,12 @@
+package nc.ui.dm.sbr;
+
+import nc.ui.trade.controller.IControllerBase;
+import nc.ui.trade.manage.BillManageUI;
+import nc.ui.trade.manage.ManageEventHandler;
+
+public class ClientEventHandler extends ManageEventHandler{
+	public ClientEventHandler(BillManageUI billUI, IControllerBase control) {
+		super(billUI, control);
+		// TODO Auto-generated constructor stub
+	}
+}
