c9638738b92dac4ca5bab1559f8aae320c82f363
liuyushi_alice@163.com <liuyushi_alice@163.com@5166a296-a8aa-909f-7fcb-6b20ff67c8c9>
2011-03-30 12:18:31 +0000

    lys

--- trunk/wds2/src/client/nc/ui/dm/plan/ClientUI.java
+++ trunk/wds2/src/client/nc/ui/dm/plan/ClientUI.java
@@ -1,7 +1,9 @@
 package nc.ui.dm.plan;
+import nc.ui.pu.pub.PuTool;
 import nc.ui.pub.ButtonObject;
 import nc.ui.pub.bill.BillEditEvent;
 import nc.ui.pub.bill.BillItem;
+import nc.ui.scm.pub.panel.RelationsCal;
 import nc.ui.trade.bill.AbstractManageController;
 import nc.ui.trade.bsdelegate.BusinessDelegator;
 import nc.ui.trade.business.HYPubBO_Client;
@@ -9,7 +11,9 @@ import nc.ui.trade.button.IBillButton;
 import nc.ui.trade.manage.BillManageUI;
 import nc.ui.trade.manage.ManageEventHandler;
 import nc.ui.wl.pub.LoginInforHelper;
+import nc.vo.hi.hi_410.PubDocVO;
 import nc.vo.pub.CircularlyAccessibleValueObject;
+import nc.vo.pub.lang.UFDouble;
 import nc.vo.scm.pu.PuPubVO;
 import nc.vo.trade.pub.IBillStatus;
 import nc.vo.wl.pub.WdsWlPubConst;
@@ -22,7 +26,7 @@ import nc.vo.wl.pub.WdsWlPubConst;
 public class ClientUI extends BillManageUI {
 
 	private static final long serialVersionUID = -3998675844592858916L;
-
+	
 	public ClientUI() {
 		super();
 	}
@@ -115,11 +119,173 @@ public class ClientUI extends BillManageUI {
 						getBillCardPanel().setHeadItem("pk_inwhouse", null);
 					}
 				}
-			}else{
-				
+			}else if(e.getPos() == BillItem.BODY){
+				//编辑存货
+				if("invcode".equalsIgnoreCase(key)){
+					afterEditWhenAssistUnit(e);
+				}else if("nplannum".equalsIgnoreCase(key)){//编辑计划数量
+					afterEditNplannum(e);
+				}else if("nassplannum".equalsIgnoreCase(key)){//编辑计划辅数量
+					afterEditNassplannum(e);
+				}else if("assunitname".equalsIgnoreCase(key)){//编辑主计量单位
+					afterEditWhenAssunit(e);
+				}
 			}
 	}
+		/**
+		 * 编辑计划数量
+		 * liuys add
+		 * @param e
+		 */
+		private void afterEditNplannum(BillEditEvent e) {
+			UFDouble convert = new UFDouble(0);
+			String hsl = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "hsl"));
+			// 计划数量
+			String nplannum = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "nplannum"));
 
+			if (hsl == null || nplannum == null) {
+//				getBillCardPanel().setCellEditable(e.getRow(), "hsl", false);
+//				getBillCardPanel().setCellEditable(e.getRow(), "nassplannum", false);
+//				getBillCardPanel().setCellEditable(e.getRow(), "assunitname", false);
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"hsl");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"nassplannum");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunitname");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunit");
+				return;
+			}
+			convert = new UFDouble(nplannum).div(new UFDouble(hsl));
+			// 设置辅信息 到界面
+			getBillCardPanel().getBillModel()
+					.setValueAt(convert, e.getRow(), "nassplannum");
+		}
+		/**
+		 * liuys  add
+		 * 编辑计划辅数量
+		 * @param e
+		 */
+		private void afterEditNassplannum(BillEditEvent e) {
+			UFDouble convert = new UFDouble(0);
+			String hsl = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "hsl"));
+			// 计划辅数量
+			String nassplannum = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "nassplannum"));
+
+			if (hsl == null || nassplannum == null) {
+//				getBillCardPanel().setCellEditable(e.getRow(), "hsl", false);
+//				getBillCardPanel().setCellEditable(e.getRow(), "nassplannum", false);
+//				getBillCardPanel().setCellEditable(e.getRow(), "assunitname", false);
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"hsl");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"nassplannum");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunitname");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunit");
+				return;
+			}
+			convert = new UFDouble(hsl).multiply(new UFDouble(nassplannum));
+			// 设置辅信息 到界面
+			getBillCardPanel().getBillModel()
+					.setValueAt(convert, e.getRow(), "nplannum");
+		}
+		
+		/**
+		 * liuys add 
+		 * for 完达山物流项目
+		 * 2011-03-30
+		 * 辅计量编辑事件 处理： # 辅计量主键编辑时触发处理 ==>> 换算率 | # 选取的计量ID是主计量ID：变换率置为1，固定换算率 | #
+		 * 由“换算率”驱动公式计算 > 步完成均处理合格数量是否可编辑 # 同步更新单据模板中的换算率和是否固定换算率属性 | 
+		 * # 更新换算率属性列可编辑性
+		 * 
+		 * @param e
+		 *            nc.ui.pub.bill.BillEditEvent
+		 */
+		private void afterEditWhenAssistUnit(BillEditEvent e) {
+			// 存货ID
+			 UFDouble convert = new UFDouble(0);
+			String sBaseID = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "pk_invbasdoc"));
+			// 辅计量主键
+			String sCassId = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "assunit"));
+
+			if (sCassId == null) {
+				getBillCardPanel().setCellEditable(e.getRow(), "hsl", false);
+				getBillCardPanel().setCellEditable(e.getRow(), "nassplannum", false);
+				getBillCardPanel().setCellEditable(e.getRow(), "assunitname", false);
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"hsl");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"nassplannum");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunitname");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunit");
+				return;
+			}
+			// 获取换算率
+			convert = nc.ui.pu.pub.PuTool.getInvConvRateValue(sBaseID, sCassId);
+			// 设置辅信息 到界面
+			getBillCardPanel().getBillModel()
+					.setValueAt(convert, e.getRow(), "hsl");
+		}
+		
+		/**
+		 * liuys add
+		 * 
+		 * 编辑主计量单位
+		 * @param e
+		 */
+		private void afterEditWhenAssunit(BillEditEvent e) {
+			// 是否固定变化率改变时要同步更改：strKeys[0]的值
+			boolean isfixed = true;
+			// 存货ID
+			 UFDouble convert = new UFDouble(0);
+			String sBaseID = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "pk_invbasdoc"));
+			// 辅计量主键
+			String sCassId = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "assunit"));
+
+			if (sCassId == null) {
+				getBillCardPanel().setCellEditable(e.getRow(), "hsl", false);
+				getBillCardPanel().setCellEditable(e.getRow(), "nassplannum", false);
+				getBillCardPanel().setCellEditable(e.getRow(), "assunitname", false);
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"hsl");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"nassplannum");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunitname");
+				getBillCardPanel().getBillModel().setValueAt(null, e.getRow(),
+						"assunit");
+				return;
+			}
+			// 获取换算率
+			convert = nc.ui.pu.pub.PuTool.getInvConvRateValue(sBaseID, sCassId);
+			// 是否固定换算率
+			isfixed = PuTool.isFixedConvertRate(sBaseID, sCassId);
+			// 设置辅信息 到界面
+			getBillCardPanel().getBillModel()
+					.setValueAt(convert, e.getRow(), "hsl");
+			// 计划数量
+			String nplannum = PuPubVO.getString_TrimZeroLenAsNull(getBillCardPanel()
+					.getBillModel().getValueAt(e.getRow(), "nplannum"));
+			if(convert == null)
+				return;
+			if(nplannum != null && convert != null){
+				UFDouble nassplannum = new UFDouble(nplannum).div(convert);
+				getBillCardPanel().getBillModel().setValueAt(nassplannum, e.getRow(), "nassplannum");
+			}
+		}
 	/**
 	 * 增加后台校验
 	 */
