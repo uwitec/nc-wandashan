00e3c633f2ec6829e158827affd44c7637f8db02
mengling.rui@163.com <mengling.rui@163.com@5166a296-a8aa-909f-7fcb-6b20ff67c8c9>
2011-06-24 14:51:47 +0000

    s

--- wdswl/src/private/nc/bs/wds/load/price/LoadPriceBO.java
+++ wdswl/src/private/nc/bs/wds/load/price/LoadPriceBO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import nc.bs.dao.BaseDAO;
 import nc.bs.trade.business.IBDBusiCheck;
+import nc.bs.wl.pub.BsUniqueCheck;
 import nc.vo.pub.AggregatedValueObject;
 import nc.vo.pub.BusinessException;
 import nc.vo.trade.pub.IBDACTION;
@@ -35,42 +36,45 @@ public class LoadPriceBO implements IBDBusiCheck {
 		if (ivo.getPk_invmandoc() == null
 				|| "".equalsIgnoreCase(ivo.getPk_invmandoc())) {
 			throw new BusinessException("请输入存货");
-		}
-		String condition = " pk_invmandoc='" + ivo.getPk_invmandoc()
-				+ "' and  isnull(dr,0)=0";
-		if (ivo.getPrimaryKey() == null
-				|| ivo.getPrimaryKey().trim().equals("")) {
-
-			List list = (List) getDao().retrieveByClause(LoadpriceVO.class,
-					condition);
-			if (list == null || list.size() == 0) {
-				return;
-			} else {
-				throw new BusinessException(" 该存货已经设置过装卸费价格 ");
-			}
-		} else {
-			// 否则执行下面的代码
-			String pkSql = " pk_loadprice='" + ivo.getPrimaryKey()
-					+ "' and  isnull(dr,0)=0";
-			List list = (List) getDao().retrieveByClause(LoadpriceVO.class,
-					pkSql);
-
-			if (list == null) {
-				return;
-			}
-			// 判断修改后的记录，是否改变了存货（即拿数据库中的记录和ui中的当前记录进行比较）
-			LoadpriceVO ivo2 = (LoadpriceVO) list.get(0);
-			if (ivo.getPk_invmandoc().equalsIgnoreCase(ivo2.getPk_invmandoc())) {
-				return;
-			}
-			List list1 = (List) getDao().retrieveByClause(LoadpriceVO.class,
-					condition);
-			if (list1 == null || list1.size() == 0) {
-				return;
-			} else {
-				throw new BusinessException(" 该存货在数据库中已经存在 ");
-			}
-		}
+		} 
+		
+		BsUniqueCheck.FieldUniqueCheck(ivo, new String[]{"pk_invmandoc","cwarehouseid"},"该仓库下已经有该存货");
+		
+//		String condition = " pk_invmandoc='" + ivo.getPk_invmandoc()
+//				+ "' and  isnull(dr,0)=0";
+//		if (ivo.getPrimaryKey() == null
+//				|| ivo.getPrimaryKey().trim().equals("")) {
+//
+//			List list = (List) getDao().retrieveByClause(LoadpriceVO.class,
+//					condition);
+//			if (list == null || list.size() == 0) {
+//				return;
+//			} else {
+//				throw new BusinessException(" 该存货已经设置过装卸费价格 ");
+//			}
+//		} else {
+//			// 否则执行下面的代码
+//			String pkSql = " pk_loadprice='" + ivo.getPrimaryKey()
+//					+ "' and  isnull(dr,0)=0";
+//			List list = (List) getDao().retrieveByClause(LoadpriceVO.class,
+//					pkSql);
+//
+//			if (list == null) {
+//				return;
+//			}
+//			// 判断修改后的记录，是否改变了存货（即拿数据库中的记录和ui中的当前记录进行比较）
+//			LoadpriceVO ivo2 = (LoadpriceVO) list.get(0);
+//			if (ivo.getPk_invmandoc().equalsIgnoreCase(ivo2.getPk_invmandoc())) {
+//				return;
+//			}
+//			List list1 = (List) getDao().retrieveByClause(LoadpriceVO.class,
+//					condition);
+//			if (list1 == null || list1.size() == 0) {
+//				return;
+//			} else {
+//				throw new BusinessException(" 该存货在数据库中已经存在 ");
+//			}
+//		}
 	}
 
 	public void dealAfter(int intBdAction, AggregatedValueObject billVo,
