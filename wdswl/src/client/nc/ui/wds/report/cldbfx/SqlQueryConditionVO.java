da9ac840351c1137faf8f78d9f5087e296ae808e
mengling.rui@163.com <mengling.rui@163.com@5166a296-a8aa-909f-7fcb-6b20ff67c8c9>
2012-08-03 01:55:02 +0000

    yf

--- wdswl/src/client/nc/ui/wds/report/cldbfx/SqlQueryConditionVO.java
+++ wdswl/src/client/nc/ui/wds/report/cldbfx/SqlQueryConditionVO.java
@@ -12,7 +12,7 @@ public abstract class SqlQueryConditionVO implements ISqlforcldbfx {
 	public static String pk_cargdoc = "pk_cargdoc";// 货位
 	public static String pk_invcl = "pk_invcl";// 存货分类
 	public static String pk_invmandoc = "pk_invmandoc";// 存货管理档案
-	public static String pk_invbasdoc = "pk_invbasdoc";//存货基本档案
+	public static String pk_invbasdoc = "pk_invbasdoc";// 存货基本档案
 	// erp库存nerpstornum、物流库存nwlstornum、虚拟订单剩余量nwlxnddnum、虚拟运单剩余量nwlxnydnum
 	public static String nerpstornum = "nerpstornum";//
 	public static String nwlstornum = "nwlstornum";
@@ -32,15 +32,19 @@ public abstract class SqlQueryConditionVO implements ISqlforcldbfx {
 		String code = "";
 		for (int i = 0; i < vos1.length; i++) {
 			code = vos1[i].getFieldCode();
-			if (pk_stordoc.equalsIgnoreCase(code)) {
+			if (pk_stordoc.equalsIgnoreCase(code) && getStorFieldName() != null) {
 				vos1[i].setFieldCode(getStorFieldName());
-			} else if (pk_cargdoc.equalsIgnoreCase(code)) {
+			} else if (pk_cargdoc.equalsIgnoreCase(code)
+					&& getCargFieldName() != null) {
 				vos1[i].setFieldCode(getCargFieldName());
-			} else if (pk_invcl.equalsIgnoreCase(code)) {
+			} else if (pk_invcl.equalsIgnoreCase(code)
+					&& getInvclFieldName() != null) {
 				vos1[i].setFieldCode(getInvclFieldName());
-			} else if (pk_invmandoc.equalsIgnoreCase(code)) {
+			} else if (pk_invmandoc.equalsIgnoreCase(code)
+					&& getInvmandocFieldName() != null) {
 				vos1[i].setFieldCode(getInvmandocFieldName());
-			} else if (pk_corp.equalsIgnoreCase(code)) {
+			} else if (pk_corp.equalsIgnoreCase(code)
+					&& getCorpFieldName() != null) {
 				vos1[i].setFieldCode(getCorpFieldName());
 			}
 
